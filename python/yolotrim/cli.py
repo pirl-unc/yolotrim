f6e9b5dbf1dacff70b560bcb60073c0540cd4389
Alex Rubinsteyn <alex.rubinsteyn@gmail.com>
2023-04-18 15:15:01 -0400

    adding argument parsing on python side

--- python/yolotrim/cli.py
+++ python/yolotrim/cli.py
@@ -1,2 +1,14 @@
+from .yolotrim import trim_fastq
+
+import argparse
+
+parser = argparse.ArgumentParser("yolotrim", description="Heuristic adapter and poly-A trimmer for Iso-Seq reads")
+parser.add_argument("--input", help="Input FASTQ file", required=True)
+parser.add_argument("--output", help="Output FASTQ file", required=True)
+
 def main():
-    print("yolotrim")
+    print("=== yolotrim ===")
+    args = parser.parse_args()
+    print("Args: %s" % (args, ))
+    trim_fastq(args.input, args.output)
+    
